85cbb8055d65ea1fc507fe7ecaefe89f6ea687bd
d03x <dadanhidyt@gmail.com>
2026-02-25 12:12:45 +0700

    feat(auth): implement modern responsive login layout in auth-layout.vue

--- resources/js/components/auth-layout.vue
+++ resources/js/components/auth-layout.vue
@@ -3,23 +3,25 @@ import ChevronLeft from './icons/ChevronLeft.vue';
 </script>
 
 <template>
-    <div class="sticky top-0 flex h-12 w-full items-center justify-start bg-orange-500 text-white">
-        <div class="container mx-auto">
-            <div class="text-2xl">
+    <div class="sticky top-0 flex h-12 w-full items-center justify-start shadow-md lg:bg-orange-500 lg:text-white">
+        <div class="container mx-auto flex items-center justify-start">
+            <button class="h-full p-2 text-2xl text-stone-500 hover:text-stone-700 active:text-stone-700 lg:hidden">
                 <ChevronLeft />
-            </div>
-            <div class="absolute inset-0 flex flex-1 items-center justify-center">
-                <h1 class="text-md font-bold lg:text-lg">Login Wuzz</h1>
+            </button>
+            <div class="absolute inset-0 flex flex-1 items-center lg:justify-center">
+                <h1 class="text-md ml-13 font-bold lg:ml-0 lg:text-lg">Login Wuzz</h1>
             </div>
         </div>
     </div>
     <div
-        class="mt-4 flex-col items-center justify-center bg-stone-100 bg-cover bg-center bg-no-repeat lg:mt-0 lg:flex lg:max-h-screen lg:min-h-screen"
+        class="mt-3 flex-col items-center justify-center bg-stone-100 bg-cover bg-center bg-no-repeat lg:mt-0 lg:flex lg:max-h-screen lg:min-h-screen"
     >
         <div class="w-full rounded-lg border-stone-200 bg-white p-3 px-5 pb-7 lg:max-w-md lg:border lg:p-6 lg:px-9">
             <div class="mb-3">
-                <h1 class="text-start text-lg font-bold">Masuk ke WuzStore</h1>
-                <p class="text-sm">Belum punya akun? <a href="/account/register" class="text-orange-500">Daftar</a></p>
+                <h1 class="text-start text-lg font-bold">
+                    <slot name="title" />
+                </h1>
+                <slot name="description" />
             </div>
             <slot />
             <slot name="footer" />
